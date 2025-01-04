9b310ab041d21e854048da97fa3749853550172a
strzprld <xdresik@gmail.com>
2025-01-04 09:40:29 +0100

    full_report function rewritten, added filtering for letters only

--- main.py
+++ main.py
@@ -25,18 +25,26 @@ def split_to_characters(text): # Split a string into characters
 def count_characters(characters): # Count the number of characters in a list
     character_count = {}
     for character in characters:
-        if character in character_count:
-            character_count[character] += 1
-        else:
-            character_count[character] = 1
+        if character.isalpha():
+             if character in character_count:
+                 character_count[character] += 1
+             else:
+                 character_count[character] = 1
     return character_count
 
-def full_report(text,path):
-   print (f"Beginning report of {path}")
-   print(f"{word_count(text)} number of words") # Prints the number of words in the text
-   for character, count in count_characters(split_to_characters(text)).items():
-         print(f"The {character} was found {count} times")
-   return full_report
+def full_report(text, path):
+
+    print(f"--Beginning report of {path}--")
+    print(f"{word_count(text)} number of words in the text") # Prints the number of words in the text
+
+    character_counts = count_characters(split_to_characters(text))
+    sorted_characters = sorted(character_counts.items(), key=lambda item: item[1], reverse=True)
+
+    for character, count in sorted_characters: # Loop through the sorted characters and their counts
+        print(f"The '{character}' character was found {count} times")
+
+    print(f"--End of report--")
+
 
 
 
