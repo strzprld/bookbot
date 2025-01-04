f6ca8d291c01bfab192d03e9f883aca91f695d72
strzprld <xdresik@gmail.com>
2025-01-04 09:21:23 +0100

    Yet to add some stuff to full report

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-def main(): # Prints the number of words in the text
+def main(): # Prints the report of the book
     path = "books/frankenstein.txt"
     text = book_text(path)
-    print(f"{word_count(text)} number of words") # Prints the number of words in the text
-    print(f"{count_characters(lowercase(text))}") # Prints the number of characters in the text
-    print(f"{character_report(count_characters(lowercase(text)))}") # Prints the report of the character count
+    full_report(text,path)
+    
 
 
 def book_text(path): # Read the text from a file
@@ -32,12 +31,13 @@ def count_characters(characters): # Count the number of characters in a list
             character_count[character] = 1
     return character_count
 
-def character_report(character_count): # Print a report of the character count
-    for character, count in character_count.items():
-        print(f"The {character} was found {count} times")
-    return character_report
+def full_report(text,path):
+   print (f"Beginning report of {path}")
+   print(f"{word_count(text)} number of words") # Prints the number of words in the text
+   for character, count in count_characters(split_to_characters(text)).items():
+         print(f"The {character} was found {count} times")
+   return full_report
 
 
 
 main() # Calls the main function
-
