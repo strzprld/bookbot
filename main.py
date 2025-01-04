cca7fd698a5cc8c31bcee5786174cce60509759e
strzprld <xdresik@gmail.com>
2025-01-04 08:07:44 +0100

    rewriting the parts of the script and adding notes for future me

--- main.py
+++ main.py
@@ -1,10 +1,25 @@
-def main():
+def main(): # Read the text from the file and print the number of words
     path = "books/frankenstein.txt"
-    with open(path) as f:
-        text = f.read()
-    print (word_count(text))
-def word_count(text): # Count the number of words in a string
-            return len(text.split())
+    text = book_text(path)
+    print(f"{word_count(text)} number of words")   # Prints the number of words
 
-main()
+
+def book_text(path):                        # Read the text from a file
+    with open(path) as f: # Opens the file
+        return f.read()
+
+def word_count(text): # Count the number of words in a string
+        words = text.split() # Split the text into words
+        return len(words) # Returns the number of words
+
+
+def lowercase(text):                # Convert a string to lowercase
+    lowercase_text = text.lower()  
+    return lowercase_text
+ 
+
+
+
+
+main() # Calls the main function
 
